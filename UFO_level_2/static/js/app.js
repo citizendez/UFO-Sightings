8c84af5e18ab427f5b0062de6a1d20d6a7fd09c9
Desiree Herschberger <citizendez@gmail.com>
2021-01-12 11:37:11 -0500

    refactor

--- UFO_level_2/static/js/app.js
+++ UFO_level_2/static/js/app.js
@@ -132,59 +132,9 @@ function populateTable(){
             //Add values to column
             cell.text(value);
         });
-        
-        
-        
-        
-        //console.log(item)
+     
     })
     
 };
 
 populateTable();
-
-/*/Input data into table
-tableData.forEach(element => {
-    //create column for DATE, isolate date data
-    var tdDate = document.createElement("td");
-    tdDate.innerText = element.datetime;
-    //create column for CITY, isolate city data
-    var tdCity = document.createElement("td");
-    tdCity.innerText = element.city;
-    //create column for STATE, isolate state data
-    var tdState = document.createElement("td");
-    tdState.innerText = element.state;
-    //create column for COUNTRY, isolate country data
-    var tdCountry = document.createElement("td");
-    tdCountry.innerText = element.country;
-    //create column for SHAPE, isolate shape data
-    var tdShape = document.createElement("td");
-    tdShape.innerText = element.shape;
-    //create column for DURATION, isolate duration data
-    var tdDuration = document.createElement("td");
-    tdDuration.innerText = element.durationMinutes;
-    //create column for COMMENTS, isolate comments data
-    var tdComments = document.createElement("td");
-    tdComments.innerText = element.comments;
-
-    //Create Row
-    var tRow = document.createElement("tr");
-    //Add columns to rows
-    //Date
-    tRow.appendChild(tdDate);
-    //City
-    tRow.appendChild(tdCity);
-    //State
-    tRow.appendChild(tdState);
-    //Country
-    tRow.appendChild(tdCountry);
-    //Shape
-    tRow.appendChild(tdShape);
-    //Duration
-    tRow.appendChild(tdDuration);
-    //Comments
-    tRow.appendChild(tdComments);
-    //Add rows to table body
-    tableBody.appendChild(tRow);
-    console.log(element);
-});*/
